67cfb970bf225685c5dce6f4b9f61ecc5ce346f7
Carlos <carlos.santos74@outlook.com>
2022-07-13 15:42:17 -0300

    feat: add initial style

---
+++ src/common/styles/videoConferenceStyle.ts
@@ -0,0 +1,19 @@
+const style = `
+  #superviz-video-wrapper {
+    position: relative;
+  }
+
+  #sv-video-wrapper iframe {
+    width: 370px;
+    height: 450px;
+
+    position: absolute;
+    top: 24px;
+    right: 24px;
+    
+    border: 1px solid #000;
+    border-radius: 4px;
+  }
+`;
+
+export default style;
