99fbe277d7ff50f781bd88374200f1d55d8a94af
Carlos <carlos.santos74@outlook.com>
2022-09-06 14:36:43 -0300

    refactor: refactor custom properties syntax

--- src/common/types/cdn.types.ts
+++ src/common/types/cdn.types.ts
@@ -1,11 +1,11 @@
-import { CommunicatorFacade } from '../../services/communicator/types';
+import { SuperVizSdk } from '../../services/communicator/types';
 import { FrameSize } from '../../services/video-conference-manager/types';
 
 import { DeviceEvent, MeetingEvent, RealtimeEvent, MeetingState } from './events.types';
 import { SuperVizSdkOptions } from './sdk-options.types';
 
 export interface SuperVizCdn {
-  init: (apiKey: string, options: SuperVizSdkOptions) => Promise<CommunicatorFacade>;
+  init: (apiKey: string, options: SuperVizSdkOptions) => Promise<SuperVizSdk>;
   MeetingEvent: typeof MeetingEvent;
   RealtimeEvent: typeof RealtimeEvent;
   FrameSize: typeof FrameSize;
