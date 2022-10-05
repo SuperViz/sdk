8c04f04958733a7f53abc092676146ed3482af04
Jaison Peres <jaisoncperes@gmail.com>
2022-10-05 07:31:29 -0300

    fix: add args to docker
    Signed-off-by: Jaison Peres <jaisoncperes@gmail.com>

--- e2e/build-e2e.sh
+++ e2e/build-e2e.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-env | sed '/^E2E_/!d' > .env
-
-docker build --no-cache -t sdk-e2e -f ./docker/e2e/Dockerfile .
+docker build --no-cache -t sdk-e2e \
+  --build-arg E2E_BASE_URL=$E2E_BASE_URL \
+  --build-arg E2E_LOCALE=$E2E_LOCALE \
+  --build-arg E2E_ROOM_ID=$E2E_ROOM_ID \
+  --build-arg E2E_USER_NAME=$E2E_USER_NAME \
+  --build-arg E2E_USER_ID=$E2E_USER_ID \
+   -f ./docker/e2e/Dockerfile .
