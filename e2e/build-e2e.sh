ea04d85a5ae7fc7196419fbc1f62ac8f1f4408ea
Jaison Peres <jaisoncperes@gmail.com>
2022-10-05 07:55:54 -0300

    fix: add eval from .env
    Signed-off-by: Jaison Peres <jaisoncperes@gmail.com>

--- e2e/build-e2e.sh
+++ e2e/build-e2e.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if test -f ".env"; then
+    eval $(cat .env | xargs)
+    echo "Using .env file"
+else
+    echo "No .env file found"
+fi
+
 docker build --no-cache -t sdk-e2e \
   --build-arg E2E_BASE_URL=$E2E_BASE_URL \
   --build-arg E2E_LOCALE=$E2E_LOCALE \
