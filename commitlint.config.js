b944ca5eaf6c76d79c30894579333a827aa3d24c
Bruno Henry <brunohenryt@hotmail.com>
2023-01-26 16:25:14 -0300

    ci: remove package.lock

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,6 +9,6 @@ module.exports = {
    * ? https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
    */
   rules: {
-    'type-enum': [2, 'always', ['chore', 'feat', 'fix']],
+    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'ci']],
   },
 };
