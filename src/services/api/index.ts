37d2b18433389e96e79381750e888f6b1090703c
Bruno Tutrut <brunohenryt@hotmail.com>
2023-05-04 08:06:30 -0300

    fix: type for apiService

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,19 +1,19 @@
 import { doRequest } from '../../common/utils';
 
 export default class ApiService {
-  static createUrl(baseUrl: string, path: string) {
+  static createUrl(baseUrl: string, path: string): string {
     return `${baseUrl}${path}`;
   }
 
   static validateApiKey(baseUrl: string, apiKey: string) {
-    const path = '/user/checkapikey';
-    const url = this.createUrl(baseUrl, path);
+    const path: string = '/user/checkapikey';
+    const url: string = this.createUrl(baseUrl, path);
     return doRequest(url, 'POST', { apiKey });
   }
 
   static fetchConfig(baseUrl: string, apiKey: string) {
-    const path = '/immersive-config';
-    const url = this.createUrl(baseUrl, path);
+    const path: string = '/immersive-config';
+    const url: string = this.createUrl(baseUrl, path);
     return doRequest(url, 'POST', { apiKey });
   }
 }
