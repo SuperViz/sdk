e55f502ed28d3b26834a5bf8628babdd112f6178
Carlos <carlos.santos74@outlook.com>
2023-06-05 11:50:42 -0300

    feat: implement new observer

--- __mocks__/observer-helper.mock.ts
+++ __mocks__/observer-helper.mock.ts
@@ -1,10 +1,11 @@
 import { jest } from '@jest/globals';
-import { ObserverHelper } from '@superviz/immersive-core';
 
-export const MOCK_OBSERVER_HELPER: ObserverHelper = {
+import { Observer } from '../src/common/utils';
+
+export const MOCK_OBSERVER_HELPER: Observer = {
   subscribe: jest.fn(),
   unsubscribe: jest.fn(),
   publish: jest.fn(),
   reset: jest.fn(),
   destroy: jest.fn(),
-} as unknown as ObserverHelper;
+} as unknown as Observer;
