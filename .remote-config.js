5e8c53fbf829a8b74933ba8d0aa9a6fe2c70c7dd
Carlos <carlos.santos74@outlook.com>
2023-08-14 12:54:41 -0300

    refactor: add default value to sync

--- .remote-config.js
+++ .remote-config.js
@@ -1,7 +1,4 @@
-module.exports = {
-  remoteConfig: {
-    apiUrl: 'https://dev.nodeapi.superviz.com',
-    conferenceLayerUrl: 'http://localhost:8080/',
-    // conferenceLayerUrl: 'https://video-frame.superviz.com/lab/index.html',
-  },
+export const remoteConfig = {
+  apiUrl: 'dev-url',
+  conferenceLayerUrl: 'frame-url',
 };
