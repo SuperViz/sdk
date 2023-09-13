91b36241ce424b697838da0bb93f4b15b8962c4f
vitorvargasdev <instalando@protonmail.com>
2023-09-13 08:02:17 -0300

    fix: presence tests

--- __mocks__/participants.mock.ts
+++ __mocks__/participants.mock.ts
@@ -1,5 +1,5 @@
 import { Avatar, Group, Participant, ParticipantType } from '../src';
-import { AblyParticipant } from "../src/services/realtime/ably/types";
+import { AblyParticipant } from '../src/services/realtime/ably/types';
 
 export const MOCK_AVATAR: Avatar = {
   model: 'unit-test-avatar-model.glb',
@@ -10,7 +10,6 @@ export const MOCK_LOCAL_PARTICIPANT: Participant = {
   id: 'unit-test-local-participant-id',
   name: 'unit-test-local-participant-name',
   color: '#000',
-  type: ParticipantType.HOST,
 };
 
 export const MOCK_GROUP: Group = {
@@ -19,14 +18,14 @@ export const MOCK_GROUP: Group = {
 };
 
 export const MOCK_ABLY_PARTICIPANT: AblyParticipant = {
-    clientId: MOCK_LOCAL_PARTICIPANT.id,
-    action: 'present',
-    connectionId: 'connection1',
-    encoding: 'h264',
-    id: 'unit-test-participant1-ably-id',
-    timestamp: new Date().getTime(),
-    data: {
-      participantId: MOCK_LOCAL_PARTICIPANT.id,
-      slotIndex: 0,
-    },
-}
+  clientId: MOCK_LOCAL_PARTICIPANT.id,
+  action: 'present',
+  connectionId: 'connection1',
+  encoding: 'h264',
+  id: 'unit-test-participant1-ably-id',
+  timestamp: new Date().getTime(),
+  data: {
+    participantId: MOCK_LOCAL_PARTICIPANT.id,
+    slotIndex: 0,
+  },
+};
