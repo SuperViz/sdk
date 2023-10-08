41bfd96c0dc8ff0ffa6f892ec31d721fcd37bb12
Carlos Santos <carlos.santos74@outlook.com>
2023-10-08 12:58:52 -0300

    refactor: remove observable methods from BaseComponent and extends Observable

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,5 +1,5 @@
 import { Group, Participant } from '../../common/types/participant.types';
-import { Logger, Observer } from '../../common/utils';
+import { Logger, Observer, Observable } from '../../common/utils';
 import config from '../../services/config';
 import { EventBus } from '../../services/event-bus';
 import { AblyRealtimeService } from '../../services/realtime';
@@ -7,9 +7,7 @@ import { ComponentNames } from '../types';
 
 import { DefaultAttachComponentOptions } from './types';
 
-export abstract class BaseComponent {
-  private observers: Record<string, Observer> = {};
-
+export abstract class BaseComponent extends Observable {
   public abstract name: ComponentNames;
   protected abstract logger: Logger;
   protected localParticipant: Participant;
@@ -77,53 +75,6 @@ export abstract class BaseComponent {
     this.isAttached = false;
 
     Object.keys(this.observers).forEach((type) => this.unsubscribe(type));
-  };
-
-  /**
-   * @function subscribe
-   * @description Subscribe to an event
-   * @param type - event type
-   * @param listener - event callback
-   * @returns {void}
-   */
-  public subscribe = (type: string, listener: Function): void => {
-    this.logger.log(`subscribed to ${type} event`);
-
-    if (!this.observers[type]) {
-      this.observers[type] = new Observer({ logger: this.logger });
-    }
-
-    this.observers[type].subscribe(listener);
-  };
-
-  /**
-   * @function unsubscribe
-   * @description Unsubscribe from an event
-   * @param type - event type
-   * @returns {void}
-   */
-  public unsubscribe = (type: string): void => {
-    this.logger.log(`unsubscribed from ${type} event`);
-
-    if (!this.observers[type]) return;
-
-    this.observers[type].reset();
-    delete this.observers[type];
-  };
-
-  /**
-   * @function publish
-   * @description Publish an event to client
-   * @param type - event type
-   * @param data - event data
-   * @returns {void}
-   */
-  protected publish = (type: string, data?: unknown): void => {
-    const hasListenerRegistered = type in this.observers;
-
-    if (!hasListenerRegistered) return;
-
-    this.observers[type].publish(data);
   };
 
   protected abstract destroy(): void;
