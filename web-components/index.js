1bd352fdeb5af65548d12b94906d73401e02e0c7
afonsovinicius <74660112+afonsovinicius@users.noreply.github.com>
2023-10-24 12:03:46 +0000

    BUILD: (2fc326e) Merge pull request #382 from SuperViz/feat/add-transition
    feat: add transition to annotation pin

--- web-components/index.js
+++ web-components/index.js
@@ -782,7 +782,7 @@ var zr=Object.defineProperty,Do=Object.defineProperties,Fo=Object.getOwnProperty
 
     border: 2px solid white;
     box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.25);
-    transition: border-color 0.2s ease-in-out;
+    transition: border-color 0.2s ease-in-out opacity 0.2s ease-in-out;
     padding: 2px;
     cursor: pointer;
   }
