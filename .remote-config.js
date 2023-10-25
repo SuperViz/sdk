faccbe3e33cd6a7191565560143b14c424ebe000
Carlos Santos <carlos.santos74@outlook.com>
2023-10-25 16:05:35 -0300

    chore(config): remove remote config file

--- .remote-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  remoteConfig: {
-    apiUrl: 'https://dev.nodeapi.superviz.com',
-    // apiUrl: 'http://localhost:3001',
-    // conferenceLayerUrl: 'http://localhost:8080/',
-    conferenceLayerUrl: 'https://video-frame.superviz.com/lab/index.html',
-  },
-};
