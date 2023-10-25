2eeaf96f3df8c1d6cc2be0eaacf4ae6b9d5dbcbb
afonsovinicius <74660112+afonsovinicius@users.noreply.github.com>
2023-10-25 18:32:55 +0000

    BUILD: (f07cad8) Merge pull request #384 from SuperViz/feat/change-comments-side
    feat: change comments side

--- web-components/comments/components/float-button.d.ts
+++ web-components/comments/components/float-button.d.ts
@@ -2,7 +2,15 @@ import { CSSResultGroup, LitElement } from 'lit';
 declare const WebComponentsBaseElement: import("../../base/types").Constructor<import("../../base/types").WebComponentsBaseInterface> & typeof LitElement;
 export declare class CommentsFloatButton extends WebComponentsBaseElement {
     static styles: CSSResultGroup[];
+    isHidden: boolean;
+    static properties: {
+        isHidden: {
+            type: BooleanConstructor;
+        };
+    };
+    constructor();
     private toggle;
+    connectedCallback(): void;
     protected render(): import("lit").TemplateResult<1>;
 }
 export {};
