36c09244e9d57b77e737802da60697a46d3f9090
Carlos Santos <carlos.santos74@outlook.com>
2023-11-06 11:57:50 -0300

    feat: splice realtime messages in 60kb and send it in every 1s

--- src/services/realtime/ably/index.ts
+++ src/services/realtime/ably/index.ts
@@ -44,7 +44,7 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
 
   private clientRoomState: Record<string, RealtimeMessage> = {};
   private clientSyncPropertiesQueue: Record<string, RealtimeMessage[]> = {};
-  private clientSyncPropertiesTimeOut: ReturnType<typeof setTimeout> = null;
+  // private clientSyncPropertiesTimeOut: ReturnType<typeof setTimeout> = null;
 
   private isReconnecting: boolean = false;
 
@@ -83,6 +83,10 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
     this.onReceiveBroadcastSync = this.onReceiveBroadcastSync.bind(this);
     this.getParticipantSlot = this.getParticipantSlot.bind(this);
     this.auth = this.auth.bind(this);
+
+    setInterval(() => {
+      this.publishClientSyncProperties();
+    }, 1000);
   }
 
   public get roomProperties() {
@@ -322,8 +326,6 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
    * @returns {void}
    */
   public setSyncProperty<T>(name: string, property: T): void {
-    const queue = this.clientSyncPropertiesQueue[name] ?? [];
-
     // closure to create the event
     const createEvent = (name: string, data: T): RealtimeMessage => {
       return {
@@ -340,12 +342,6 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
       this.throw('Message too long, the message limit size is 10kb.');
     }
 
-    // if the queue is too big, publish before add more events
-    if (this.isMessageTooBig(queue)) {
-      this.logger.log('REALTIME', 'Message queue too big, publishing');
-      this.publishClientSyncProperties();
-    }
-
     this.logger.log('adding to queue', name, property);
 
     if (!this.clientSyncPropertiesQueue[name]) {
@@ -353,12 +349,6 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
     }
 
     this.clientSyncPropertiesQueue[name].push(createEvent(name, property));
-
-    if (!this.clientSyncPropertiesTimeOut) {
-      this.clientSyncPropertiesTimeOut = setTimeout(() => {
-        this.publishClientSyncProperties();
-      }, 1000);
-    }
   }
 
   /**
@@ -453,16 +443,25 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
     if (this.state !== RealtimeStateTypes.CONNECTED) return;
 
     Object.keys(this.clientSyncPropertiesQueue).forEach((name) => {
-      const eventQueue = this.clientSyncPropertiesQueue[name];
-      this.clientSyncPropertiesQueue[name] = [];
+      this.clientSyncPropertiesQueue[name] = this.clientSyncPropertiesQueue[name].sort(
+        (a, b) => a.timestamp - b.timestamp,
+      );
 
-      if (!eventQueue.length) return;
+      if (!this.clientSyncPropertiesQueue[name].length) return;
 
-      this.clientSyncChannel.publish(name, eventQueue);
+      const { data, lengthToBeSplitted } = this.spliceArrayBySize(
+        this.clientSyncPropertiesQueue[name],
+      );
+
+      const eventQueue = data;
+      this.clientSyncPropertiesQueue[name].splice(0, lengthToBeSplitted);
+
+      this.clientSyncChannel.publish(name, eventQueue, (error) => {
+        if (!error) return;
+
+        this.logger.log('REALTIME', 'Error in publish client sync properties', error.message);
+      });
     });
-
-    clearTimeout(this.clientSyncPropertiesTimeOut);
-    this.clientSyncPropertiesTimeOut = null;
   };
 
   /**
@@ -1242,6 +1241,47 @@ export default class AblyRealtimeService extends RealtimeService implements Ably
     }
     return false;
   };
+
+  /**
+   * @function spliceArrayBySize
+   * @description splits an array into smaller arrays by size
+   * @param {Array<unknown>} array
+   * @param {number} maxSizeKB
+   */
+  private spliceArrayBySize(array: Array<unknown>, maxSizeKB: number = MESSAGE_SIZE_LIMIT) {
+    const sizeOfElementInKB = (element: unknown) => {
+      const elementString = JSON.stringify(element);
+      const size = new TextEncoder().encode(elementString).length;
+
+      return size;
+    };
+
+    let currentSizeKB = 0;
+    const splicedArrays = [];
+    let currentSlice = [];
+
+    for (let i = 0; i < array.length; i++) {
+      const elementSizeKB = sizeOfElementInKB(array[i]);
+
+      if (currentSizeKB + elementSizeKB <= maxSizeKB) {
+        currentSlice.push(array[i]);
+        currentSizeKB += elementSizeKB;
+      } else {
+        splicedArrays.push(currentSlice);
+        currentSlice = [array[i]];
+        currentSizeKB = elementSizeKB;
+      }
+    }
+
+    if (currentSlice.length > 0) {
+      splicedArrays.push(currentSlice);
+    }
+
+    return {
+      data: splicedArrays[0],
+      lengthToBeSplitted: splicedArrays[0].length,
+    };
+  }
 
   /** Comments */
   private onCommentsChannelUpdate = (message: Ably.Types.Message): void => {
