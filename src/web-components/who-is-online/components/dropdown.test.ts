99ed9479b539b583a0ff95802ad537b49881cb95
Ian Silva <raspincel@gmail.com>
2023-11-11 18:05:53 -0300

    feat: add tests for who-is-online dropdown

--- src/web-components/who-is-online/components/dropdown.test.ts
+++ src/web-components/who-is-online/components/dropdown.test.ts
@@ -224,5 +224,50 @@ describe('dropdown', () => {
     );
   });
 
-  test('should call options handler when selecting an option', async () => {});
+  test('should not render participants when there is no participant', async () => {
+    createEl({ position: 'bottom-right', align: 'left' });
+
+    await sleep();
+    expect(dropdownMenu()?.children?.length).toBe(0);
+  });
+
+  test('should render participants when there is participant', async () => {
+    createEl({ position: 'bottom-right', align: 'left', participants: mockParticipants });
+
+    await sleep();
+
+    expect(dropdownMenu()?.children?.length).toBe(1);
+  });
+
+  test('should change selected participant when click on it', async () => {
+    createEl({ position: 'bottom-right', align: 'left', participants: mockParticipants });
+
+    await sleep();
+
+    const participant = element()?.shadowRoot?.querySelector(
+      '.who-is-online-dropdown__content',
+    ) as HTMLElement;
+
+    participant.click();
+
+    await sleep();
+
+    expect(element()?.['selected']).toBe(mockParticipants[0].id);
+  });
+
+  // @TODO: create tests in V2 (dropdownOptionsHandler does not have an implementation yet)
+  test('', async () => {
+    const event = new CustomEvent('selected');
+    createEl({
+      position: 'bottom-right',
+      align: 'left',
+      participants: mockParticipants,
+    });
+
+    await sleep();
+
+    const dropdown = element()?.shadowRoot?.querySelector('superviz-dropdown') as HTMLElement;
+
+    dropdown.dispatchEvent(event);
+  });
 });
