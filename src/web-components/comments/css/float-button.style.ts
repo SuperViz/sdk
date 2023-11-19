4a67aabe1be09dbc1db2364b91f13d9dd93596d8
Carlos Santos <carlos.santos74@outlook.com>
2023-11-19 17:04:09 -0300

    feat: add custom colors variables to web-components

--- src/web-components/comments/css/float-button.style.ts
+++ src/web-components/comments/css/float-button.style.ts
@@ -14,7 +14,7 @@ export const floatButtonStyle = css`
     gap: 4px;
     border: none;
 
-    background-color: white;
+    background-color: rgba(var(--sv-white));
     box-shadow: 2px 2px 15px 0px rgba(0, 0, 0, 0.2);
     color: rgb(var(--sv-gray-600));
     transition: all 300ms;
