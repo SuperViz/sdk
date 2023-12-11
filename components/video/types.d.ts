53024630c861a797f2907a8f95f48a42ab276d0f
carlossantos74 <carlos.santos74@outlook.com>
2023-12-11 10:13:49 +0000

    BUILD: (f8c407a) Merge pull request #454 from SuperViz/chore/remove-collaboration-false
    chore: temporarily remove collaboration mode

--- components/video/types.d.ts
+++ components/video/types.d.ts
@@ -21,7 +21,6 @@ export interface VideoComponentOptions {
     userType?: ParticipantType | `${ParticipantType}`;
     participantType?: ParticipantType | `${ParticipantType}`;
     collaborationMode?: {
-        enabled?: boolean;
         position?: CamerasPosition | `${CamerasPosition}`;
         modalPosition?: LayoutPosition | `${LayoutPosition}`;
         initialView?: LayoutMode | `${LayoutMode}`;
