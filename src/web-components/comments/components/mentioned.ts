3171d7e46dff9097682aca0aa0381591688ea623
vitorvargasdev <instalando@protonmail.com>
2023-12-14 14:48:33 -0300

    feat(wip): add components to mention list

---
+++ src/web-components/comments/components/mentioned.ts
@@ -0,0 +1,24 @@
+import { CSSResultGroup, LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+
+import { WebComponentsBase } from '../../base';
+import { annotationItemStyle } from '../css';
+
+const WebComponentsBaseElement = WebComponentsBase(LitElement);
+const styles: CSSResultGroup[] = [WebComponentsBaseElement.styles, annotationItemStyle];
+
+@customElement('superviz-comments-mentioned')
+export class CommentsMentioned extends WebComponentsBaseElement {
+  static styles = styles;
+
+  static properties = {
+    //
+  };
+
+  protected render() {
+    return html`
+      // ...
+    `;
+  }
+}
