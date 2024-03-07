3f9b99d65db2d6d8b57dbb242f2c56c70f3a932b
Ian Silva <raspincel@gmail.com>
2024-03-07 11:20:56 -0300

    fix: unsubscribe from subjects when destroying room

--- src/common/utils/use-store.ts
+++ src/common/utils/use-store.ts
@@ -1,6 +1,6 @@
 import { PublicSubject } from '../../services/stores/common/types';
 import { useGlobalStore } from '../../services/stores/global';
-import { Store, StoreType, StoresTypes } from '../types/stores.types';
+import { Store, StoreType } from '../types/stores.types';
 
 const stores = {
   [StoreType.GLOBAL]: useGlobalStore,
--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -79,6 +79,7 @@ export abstract class BaseComponent extends Observable {
     this.logger.log('detached');
     this.publish(ComponentLifeCycleEvent.UNMOUNT);
     this.destroy();
+    this.unsubscribeFrom.forEach((unsubscribe) => unsubscribe(this));
 
     Object.values(this.observers).forEach((observer) => {
       observer.reset();
