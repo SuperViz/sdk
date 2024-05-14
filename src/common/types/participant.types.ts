b1753c47ef282516d4f3c553738d52a744eaf27f
Carlos Santos <carlos.santos74@outlook.com>
2024-05-14 18:26:11 -0300

    fix: avatar image and model url are optionals

--- src/common/types/participant.types.ts
+++ src/common/types/participant.types.ts
@@ -41,8 +41,8 @@ export interface Group {
 }
 
 export interface Avatar {
-  model3DUrl: string;
-  imageUrl: string;
+  model3DUrl?: string;
+  imageUrl?: string;
 }
 
 export type ParticipantByCommentsApi = {
