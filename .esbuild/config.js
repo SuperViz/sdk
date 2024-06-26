ba408d258ea66feb69a07431e66c493931b676f3
Carlos <carlos.santos74@outlook.com>
2024-06-26 10:17:55 -0300

    fix: remove many entrypoints and build based on index

--- .esbuild/config.js
+++ .esbuild/config.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const { style } = require('./plugins/style-loader');
-const glob = require('glob');
 
 const entries = Object.entries(process.env).filter((key) => key[0].startsWith('SDK_'));
 const env = Object.fromEntries(entries);
-const entryPoints = glob
-  .sync('./src/**/*.ts')
-  .filter((file) => !file.endsWith('.d.ts') && !file.endsWith('.test.ts'));
 
 const config = {
-  entryPoints,
+  entryPoints: ['./src/index.ts'],
   loader: {
     '.png': 'file',
     '.svg': 'file',
