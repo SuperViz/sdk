715e547d347d2229a793de74cf12357d60b0c80a
Ian Silva <raspincel@gmail.com>
2024-08-12 06:54:47 -0300

    feat: improve typing in realtime

--- src/components/realtime/types.ts
+++ src/components/realtime/types.ts
@@ -22,10 +22,32 @@ export type RealtimeData = {
   payload: any;
 };
 
-export type RealtimeMessage = {
+export type RealtimeMessage<T = unknown> = {
   name: string;
   connectionId: string;
   participantId: string | null;
-  data: unknown;
+  data: T;
   timestamp: number;
 };
+
+type ChannelEvents = RealtimeChannelEvent | `${RealtimeChannelEvent}`;
+type ComponentEvents = RealtimeComponentEvent | `${RealtimeComponentEvent}`;
+
+type ChannelState = RealtimeChannelState | `${RealtimeChannelState}`;
+type ComponentState = RealtimeComponentState | `${RealtimeComponentState}`;
+
+type States = ChannelState | ComponentState;
+
+export type Callback<T> = (data: T extends States ? T : RealtimeMessage<T>) => void;
+
+export type RealtimeChannelSubscribe = {
+  <T>(event: ChannelEvents, callback?: Callback<ChannelState>): void;
+  <T>(event: string, callback?: Callback<T>): void;
+};
+
+export type RealtimeComponentSubscribe = {
+  <T>(event: ComponentEvents, callback?: Callback<ComponentState>): void;
+  <T>(event: string, callback?: Callback<T>): void;
+};
+
+export type RealtimePublish = <T>(event: string, data: T) => void;
